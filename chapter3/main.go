d77a47ecb57dd45bd13fcf6ed0d5b04691915959
ivan <79854463009@yandex.ru>
2020-10-04 12:21:03 +0300

    add the last changes

--- chapter3/main.go
+++ chapter3/main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // Задача состоит в следующем: на стандартный ввод подаются целые числа в диапазоне 0-100, каждое число подается на стандартный ввод с новой
@@ -29,6 +31,10 @@ func function1() {
 	}
 }
 
+// На стандартный ввод подаются данные о студентах университетской группы в формате JSON:
+// В сведениях о каждом студенте содержится информация о полученных им оценках (Rating).
+// Требуется прочитать данные, и рассчитать среднее количество оценок, полученное студентами группы.
+// Ответ на задачу требуется записать на стандартный вывод в формате JSON в следующей форме:
 type Student struct {
 	LastName, FirstName, MiddleName, Birthday, Address, Phone string
 	Rating                                                    []int
@@ -59,7 +65,7 @@ func (r *Response) getAverageFromStudent(students []Student) {
 	r.Average = float32(result) / float32(len(arr))
 }
 
-func main() {
+func function2() {
 
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
@@ -77,5 +83,98 @@ func main() {
 
 	respB, _ := json.MarshalIndent(resp, "", "    ")
 	fmt.Println(string(respB))
+}
+
+// На стандартный ввод подается строковое представление даты и времени в следующем формате:
+// 1986-04-16T05:20:00+06:00
+// Ваша задача конвертировать эту строку в Time, а затем вывести в формате UnixDate:
+// Wed Apr 16 05:20:00 +0600 1986
+
+func function3(line string) {
+	now, _ := time.Parse(time.RFC3339, line)
+	fmt.Println(now.Format(time.RubyDate))
+
+}
+
+// На стандартный ввод подается строковое представление даты и времени определенного события в следующем формате:
+// 2020-05-15 08:00:00
+// Если время события до обеда (13-00), то ничего менять не нужно, достаточно вывести дату на стандартный вывод в том же формате.
+// Если же событие должно произойти после обеда, необходимо перенести его на то же время на следующий день, а затем вывести на стандартный вывод в том же формате.
+func function4(line string) {
+	timeFormat := "2006-01-02 15:04:05"
+	date, err := time.Parse(timeFormat, line)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if date.Hour() > 13 {
+
+		date = date.Add(time.Hour * 24)
+	}
+	fmt.Println(date.Format(timeFormat))
+
+}
+
+/*
+На стандартный ввод подается строковое представление двух дат, разделенных запятой (формат данных смотрите в примере).
+Необходимо преобразовать полученные данные в тип Time, а затем вывести продолжительность периода между меньшей и большей датами.
+Sample Input:
+13.03.2018 14:00:15,12.03.2018 14:00:15
+*/
+func function5(lineTimes string) {
+	timeFormat := "02.01.2006 15:04:05"
+	times := strings.Split(lineTimes, ",")
+	time1, err1 := time.Parse(timeFormat, times[0])
+	time2, err2 := time.Parse(timeFormat, times[1])
+	if err1 != nil || err2 != nil {
+		fmt.Println(err1, err2)
+	}
+	var dur time.Duration
+
+	if time1.After(time2) {
+		dur = time1.Sub(time2)
+
+	} else {
+		dur = time2.Sub(time1)
+
+	}
+	fmt.Println(dur)
+
+}
+
+// Напишите функцию которая принимает канал и число N типа int. Необходимо вернуть значение N+1 в канал.
+// Функция должна называться task().
+func task(c chan int, N int) {
+	c <- N + 1
+}
+
+// Напишите функцию которая принимает канал и строку. Необходимо отправить эту же строку в заданный канал 5 раз, добавив к ней пробел.
+// Функция должна называться task2().
+func task2(c chan string, s string) {
+	for i := 0; i < 5; i++ {
+		c <- s + " "
+	}
+}
+func squares(c chan int) {
+	for i := 0; i <= 100; i++ {
+		c <- i
+		fmt.Println("<Squares", i)
+	}
+	close(c)
+}
+
+func main() {
+	fmt.Println("main() started")
+
+	c := make(chan int, 10)
+
+	go squares(c)
+
+	for val := range c {
+		fmt.Println("> Main", val)
+
+	}
+
+	fmt.Println("main() stoped")
 
 }
